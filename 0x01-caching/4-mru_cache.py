9fd0ccece1a2852a8b89b3e200aceb46766588bb
Tweek-Byte <zaka4396@gmail.com>
2024-08-21 16:31:15 -0700

    t5 fix

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python3
-"""MRU Caching"""
+"""MRU caching module."""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """MRU Caching"""
-
+    """OBJ that retrieves items from dic using MRU logic"""
     def __init__(self):
+        """Caching init."""
         super().__init__()
-        self.use_order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """put meth"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                mru_key = self.use_order.pop()
-                print("DISCARD: {}".format(mru_key))
-                del self.cache_data[mru_key]
+        """Adds an item to the cache."""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
             self.cache_data[key] = item
-            if key in self.use_order:
-                self.use_order.remove(key)
-            self.use_order.append(key)
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """get meth"""
-        if key is None or key not in self.cache_data:
-            return None
-
-        if key in self.use_order:
-            self.use_order.remove(key)
-        self.use_order.append(key)
-
-        return self.cache_data[key]
+        """Retrieves an item by key."""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
