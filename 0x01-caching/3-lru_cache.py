2d02ce3890f0bcfbc732b2e171965b5c560f0261
Tweek-Byte <zaka4396@gmail.com>
2024-08-21 16:33:50 -0700

    t4 fix

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
 """LRU Caching"""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ LRU caching class """
+    """OBJ that stores & retrieves from a dict using LRU logic"""
     def __init__(self):
+        """Caching init."""
         super().__init__()
-        self.use_order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """put meth"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                lru_key = self.use_order.pop(0)
-                print("DISCARD: {}".format(lru_key))
-                del self.cache_data[lru_key]
+        """Adds an item to the cache."""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
             self.cache_data[key] = item
-            if key in self.use_order:
-                self.use_order.remove(key)
-            self.use_order.append(key)
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """get meth"""
-        if key is None or key not in self.cache_data:
-            return None
-
-        if key in self.use_order:
-            self.use_order.remove(key)
-        self.use_order.append(key)
-
-        return self.cache_data[key]
+        """Retrieves an item by key."""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
